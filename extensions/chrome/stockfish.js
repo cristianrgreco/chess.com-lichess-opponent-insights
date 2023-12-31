2ad76805dee731b88a1e3581dd607437743ff275
Cristian Greco <cristianrgreco@gmail.com>
2023-12-31 15:07:51 +0000

    Remove console.log

--- extensions/chrome/stockfish.js
+++ extensions/chrome/stockfish.js
@@ -13,7 +13,6 @@ const engine = new Worker("./lib/stockfish-nnue-16.js");
 
 function setupStockfishMessageHandler(port) {
   engine.onmessage = (event) => {
-    console.log(event);
     const parts = event.data.split(" ");
 
     if (parts[0] === "info" && parts.includes("score")) {
