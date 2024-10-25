d13874732a1774d1865eb7aa289b40f1c7c3f887
Cristian Greco <cristianrgreco@gmail.com>
2024-10-25 21:44:42 +0100

    Move CSS around to right places

--- extensions/chrome-vite-react/src/lichess/LichessApp.jsx
+++ extensions/chrome-vite-react/src/lichess/LichessApp.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import * as api from "./api.js";
+import "./LichessApp.css";
 import "chart.js/auto";
 import AuthComponent from "./components/AuthComponent";
 import EloRangeComponent from "./components/EloRangeComponent";
