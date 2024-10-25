7e2be051d48f0f0815a8c85f31e3c80312de6f11
Cristian Greco <cristianrgreco@gmail.com>
2024-10-25 16:03:54 +0100

    Progress reactifiying app

--- extensions/chrome-vite-react/src/lichess/LichessApp.jsx
+++ extensions/chrome-vite-react/src/lichess/LichessApp.jsx
@@ -1,8 +1,8 @@
-import logo from "../logo_128x128.png";
 import Chart from "chart.js/auto";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 import { useEffect, useState } from "react";
 import * as api from "./api.js";
+import AuthComponent from "./components/AuthComponent";
 
 const SUPPORTED_GAME_TYPES = ["bullet", "blitz", "rapid", "classical"];
 
@@ -11,12 +11,27 @@ export default function LichessApp({ port, gameInfo: { user, opponent, opponentC
   const [accessToken, setAccessToken] = useState(null);
   const [userAnalytics, setUserAnalytics] = useState(null);
   const [opponentNotes, setOpponentNotes] = useState(null);
+  const [savingOpponentNotes, setSavingOpponentNotes] = useState(false);
   const [error, setError] = useState(null);
 
   const style = getComputedStyle(document.body);
   const fontColour = style.getPropertyValue("--color");
   const successColour = style.getPropertyValue("--success");
   const errorColour = style.getPropertyValue("--error");
+
+  const actions = (message) => ({
+    GET_LICHESS_ACCESS_TOKEN: () => {
+      if (message.payload) {
+        setAccessToken(message.payload.value);
+      }
+    },
+    AUTH_LICHESS: () => {
+      setAccessToken(message.payload.value);
+    },
+    GET_PREFERENCES: () => {
+      onRetrievePreferences(message.payload);
+    },
+  });
 
   function onClickAuthoriseWithLichess() {
     port.postMessage({ action: "AUTH_LICHESS", payload: { user } });
@@ -68,13 +83,12 @@ export default function LichessApp({ port, gameInfo: { user, opponent, opponentC
 
   function saveOpponentNotes() {
     console.log("Saving opponent notes...");
-    const saveButton = document.querySelector("#ca_save_opponent_notes_form button[type=submit]");
-    saveButton.setAttribute("disabled", "disabled");
+    setSavingOpponentNotes(true);
     api
-      .saveOpponentNotes(user, opponent, document.querySelector("#ca_opponent_notes").value)
+      .saveOpponentNotes(user, opponent, opponentNotes)
       .then(() => console.log("Saved opponent notes"))
       .catch((response) => setError("Failed to save opponent notes"))
-      .finally(() => saveButton.removeAttribute("disabled"));
+      .finally(() => setSavingOpponentNotes(false));
   }
 
   function renderAnalytics(response) {
@@ -403,28 +417,6 @@ export default function LichessApp({ port, gameInfo: { user, opponent, opponentC
   }
 
   useEffect(() => {
-    if (accessToken) {
-      port.postMessage({ action: "GET_PREFERENCES" });
-      fetchUserAnalytics();
-      fetchOpponentNotes();
-    }
-  }, [accessToken]);
-
-  useEffect(() => {
-    const actions = (message) => ({
-      GET_LICHESS_ACCESS_TOKEN: () => {
-        if (message.payload) {
-          setAccessToken(message.payload.value);
-        }
-      },
-      AUTH_LICHESS: () => {
-        setAccessToken(message.payload.value);
-      },
-      GET_PREFERENCES: () => {
-        onRetrievePreferences(message.payload);
-      },
-    });
-
     port.onMessage.addListener((message) => {
       const action = actions(message)[message.action];
       if (action) {
@@ -433,49 +425,76 @@ export default function LichessApp({ port, gameInfo: { user, opponent, opponentC
         console.log(`Unhandled message received: ${message.action}`);
       }
     });
-
     port.postMessage({ action: "GET_LICHESS_ACCESS_TOKEN" });
   }, []);
+
+  useEffect(() => {
+    if (accessToken) {
+      port.postMessage({ action: "GET_PREFERENCES" });
+      fetchUserAnalytics();
+      fetchOpponentNotes();
+    }
+  }, [accessToken]);
 
   if (!SUPPORTED_GAME_TYPES.includes(gameType)) {
     console.log(`Unsupported game type ${gameType}. Not rendering.`);
     return null;
   }
 
-  return (
-    <div className="ca_container_root">
-      {error ? (
-        <div className="ca_error">
-          <span className="ca_error_message">{error}</span>
-        </div>
-      ) : null}
-      {accessToken ? (
+  if (!accessToken) {
+    return (
+      <div className="ca_container_root">
+        <AuthComponent onClickAuthorise={onClickAuthoriseWithLichess} />
+      </div>
+    );
+  }
+
+  if (accessToken) {
+    return (
+      <div className="ca_container_root">
+        {error ? (
+          <div className="ca_error">
+            <span className="ca_error_message">{error}</span>
+          </div>
+        ) : null}
         <div className="ca_container">
           <div className="ca_section ca_opponent_info">
             <div className="ca_elo_range">
               <div className="ca_elo_range_lowest">
                 <div
-                  className={`ca_elo_range_lowest_value ca_placeholder ${userAnalytics ? "" : "ca_placeholder_enabled"}`}
+                  className={`ca_elo_range_lowest_value ca_placeholder ${
+                    userAnalytics ? "" : "ca_placeholder_enabled"
+                  }`}
                 >
                   ????
                 </div>
               </div>
               <div className="ca_elo_range_current">
                 <div
-                  className={`ca_elo_range_current_value ca_placeholder ${userAnalytics ? "" : "ca_placeholder_enabled"}`}
+                  className={`ca_elo_range_current_value ca_placeholder ${
+                    userAnalytics ? "" : "ca_placeholder_enabled"
+                  }`}
                 >
                   ????
                 </div>
               </div>
               <div className="ca_elo_range_highest">
                 <div
-                  className={`ca_elo_range_highest_value ca_placeholder ${userAnalytics ? "" : "ca_placeholder_enabled"}`}
+                  className={`ca_elo_range_highest_value ca_placeholder ${
+                    userAnalytics ? "" : "ca_placeholder_enabled"
+                  }`}
                 >
                   ????
                 </div>
               </div>
             </div>
-            <div style={{ display: "flex", alignItems: "center", fontSize: "12px" }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                fontSize: "12px",
+              }}
+            >
               <div className="ca_opponent_info_section" title="Puzzle Rating">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -528,7 +547,9 @@ export default function LichessApp({ port, gameInfo: { user, opponent, opponentC
               Openings
             </span>
             <span
-              className={`ca_tab ca_tab_icon ca_notes_tab_trigger ${currentTab === "NOTES" ? "ca_active" : ""} ${opponentNotes ? "ca_green_colour" : ""}`}
+              className={`ca_tab ca_tab_icon ca_notes_tab_trigger ${currentTab === "NOTES" ? "ca_active" : ""} ${
+                opponentNotes ? "ca_green_colour" : ""
+              }`}
               onClick={(e) => setAndSaveCurrentTab("NOTES")}
             >
               <svg
@@ -581,12 +602,12 @@ export default function LichessApp({ port, gameInfo: { user, opponent, opponentC
                   className={`ca_textarea ca_placeholder ${userAnalytics ? "" : "ca_placeholder_enabled"}`}
                   value={`${opponentNotes}` ?? ""}
                   onChange={(e) => setOpponentNotes(e.target.value)}
-                >
-                </textarea>
+                ></textarea>
               </div>
               <div>
                 <button
                   type="submit"
+                  disabled={savingOpponentNotes}
                   className={`ca_button ca_btn-win ca_placeholder ${userAnalytics ? "" : "ca_placeholder_enabled"}`}
                 >
                   Save
@@ -595,39 +616,7 @@ export default function LichessApp({ port, gameInfo: { user, opponent, opponentC
             </form>
           </div>
         </div>
-      ) : (
-        <div className="ca_auth_container">
-          <div className="ca_title">Lichess Opponent Information</div>
-          <div className="ca_auth_content_container">
-            <img id="ca_logo" alt="Logo" src={chrome.runtime.getURL(logo)} />
-            <div>
-              <p>
-                This extension uses the{" "}
-                <a href="https://lichess.org/api" target="_blank">
-                  Lichess API
-                </a>{" "}
-                to fetch opponent information.
-              </p>
-              <p style={{ marginBottom: 0 }}>You will be prompted to re-authorise when the access token expires.</p>
-            </div>
-          </div>
-          <button
-            id="auth_lichess_btn"
-            className="ca_button ca_button_large ca_btn-win"
-            onClick={onClickAuthoriseWithLichess}
-          >
-            <span> Authorise with Lichess </span>
-            <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M5 12V6C5 5.44772 5.44772 5 6 5H18C18.5523 5 19 5.44772 19 6V18C19 18.5523 18.5523 19 18 19H12M8.11111 12H12M12 12V15.8889M12 12L5 19"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
-        </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
 }
---
+++ extensions/chrome-vite-react/src/lichess/components/AuthComponent.jsx
@@ -0,0 +1,34 @@
+import logo from "../../logo_128x128.png";
+import "./AuthComponent.css";
+
+export default function AuthComponent({ onClickAuthorise }) {
+  return (
+    <div className="ca_auth_container">
+      <div className="ca_title">Lichess Opponent Information</div>
+      <div className="ca_auth_content_container">
+        <img id="ca_logo" alt="Logo" src={chrome.runtime.getURL(logo)} />
+        <div>
+          <p>
+            <span>This extension uses the </span>
+            <a href="https://lichess.org/api" target="_blank">
+              Lichess API
+            </a>
+            <span> to fetch opponent information.</span>
+          </p>
+          <p style={{ marginBottom: 0 }}>You will be prompted to re-authorise when the access token expires.</p>
+        </div>
+      </div>
+      <button id="auth_lichess_btn" className="ca_button ca_button_large ca_btn-win" onClick={onClickAuthorise}>
+        <span> Authorise with Lichess</span>
+        <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M5 12V6C5 5.44772 5.44772 5 6 5H18C18.5523 5 19 5.44772 19 6V18C19 18.5523 18.5523 19 18 19H12M8.11111 12H12M12 12V15.8889M12 12L5 19"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
