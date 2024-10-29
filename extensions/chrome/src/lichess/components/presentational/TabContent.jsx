70b7682b5a7f78b38579598c720ae6f3239148db
Cristian Greco <cristianrgreco@gmail.com>
2024-10-29 20:22:33 +0000

    Fix Lichess opponent notes height

--- extensions/chrome/src/lichess/components/presentational/TabContent.jsx
+++ extensions/chrome/src/lichess/components/presentational/TabContent.jsx
@@ -1,3 +1,5 @@
+import "./TabContent.css";
+
 export default function TabContent({ label, currentTab, children }) {
   return <div className={`ca_section ca_tab_section ${currentTab !== label ? "ca_hidden" : ""}`}>{children}</div>;
 }
