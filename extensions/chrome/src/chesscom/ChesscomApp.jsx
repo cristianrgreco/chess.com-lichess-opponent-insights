dd5cd364d0dcb1bdbaff86d2bce173f163e6c683
Cristian Greco <cristianrgreco@gmail.com>
2025-05-02 18:01:58 +0100

    Looking nice

--- extensions/chrome/src/chesscom/ChesscomApp.jsx
+++ extensions/chrome/src/chesscom/ChesscomApp.jsx
@@ -32,41 +32,43 @@ export default function ChesscomApp({ port, gameInfo }) {
           onClick={() => setVisible(!visible)}
           className={`ca_chesscom__collapse ${visible ? "ca_chesscom__collapse-visible" : "ca_chesscom__collapse-invisible"}`}
         ></div>
-        <div className="ca_chesscom__header">
-          <img alt="Logo" src={chrome.runtime.getURL(logo)} />
-          <h1 className="ca_chesscom__header-title">
-            <span className="ca_chesscom__header-title-opponent-name">{gameInfo.opponent}</span>
-            <span>Insights</span>
-          </h1>
-        </div>
-        <div className="ca_chesscom__summary">
-          <EloRange isLoading={!userAnalytics} userAnalytics={userAnalytics} />
-          <div className="ca_opponent_info_section" title="Puzzle Rating">
-            <PuzzleIcon width="16" height="16" />
-            <PuzzleRating userAnalytics={userAnalytics} />
+        <div className="ca_chesscom__content">
+          <div className="ca_chesscom__header">
+            <img className="ca_chesscom__collapse-logo" alt="Logo" src={chrome.runtime.getURL(logo)} />
+            <h1 className="ca_chesscom__header-title">
+              <span className="ca_chesscom__header-title-opponent-name">{gameInfo.opponent}</span>
+              <span>Insights</span>
+            </h1>
           </div>
-          <div className="ca_opponent_info_section" title="Disconnects">
-            <DisconnectIcon width="16" height="16" />
-            <Disconnects userAnalytics={userAnalytics} />
+          <div className="ca_chesscom__summary">
+            <EloRange isLoading={!userAnalytics} userAnalytics={userAnalytics} />
+            <div className="ca_opponent_info_section" title="Puzzle Rating">
+              <PuzzleIcon width="16" height="16" />
+              <PuzzleRating userAnalytics={userAnalytics} />
+            </div>
+            <div className="ca_opponent_info_section" title="Disconnects">
+              <DisconnectIcon width="16" height="16" />
+              <Disconnects userAnalytics={userAnalytics} />
+            </div>
+            <div className="ca_opponent_info_section" title="Streak">
+              <span className={`icon-font-chess ${gameInfo.gameType} tabs-icon`} style={{ fontSize: "16px" }}></span>
+              <Streak userAnalytics={userAnalytics} />
+            </div>
           </div>
-          <div className="ca_opponent_info_section" title="Streak">
-            <span className={`icon-font-chess ${gameInfo.gameType} tabs-icon`} style={{ fontSize: "16px" }}></span>
-            <Streak userAnalytics={userAnalytics} />
+          <div className="ca_charts_container">
+            <StatsChart isLoading={userAnalytics === null} userAnalytics={userAnalytics} height={100} />
+            <OpeningsChart isLoading={userAnalytics === null} userAnalytics={userAnalytics} />
+            <MoveTimesChart isLoading={userAnalytics === null} userAnalytics={userAnalytics} height={100} />
           </div>
-        </div>
-        <div className="ca_charts_container">
-          <StatsChart isLoading={userAnalytics === null} userAnalytics={userAnalytics} height={100} />
-          <OpeningsChart isLoading={userAnalytics === null} userAnalytics={userAnalytics} />
-          <MoveTimesChart isLoading={userAnalytics === null} userAnalytics={userAnalytics} height={100} />
-        </div>
-        <div className="ca_chesscom__opponent-notes">
-          <OpponentNotesContainer
-            shouldInit={true}
-            gameInfo={gameInfo}
-            setError={setError}
-            opponentNotes={opponentNotes}
-            setOpponentNotes={setOpponentNotes}
-          />
+          <div className="ca_chesscom__opponent-notes">
+            <OpponentNotesContainer
+              shouldInit={true}
+              gameInfo={gameInfo}
+              setError={setError}
+              opponentNotes={opponentNotes}
+              setOpponentNotes={setOpponentNotes}
+            />
+          </div>
         </div>
       </div>
     </PageStylesWrapper>
