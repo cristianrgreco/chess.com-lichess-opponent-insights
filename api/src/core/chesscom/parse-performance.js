264f4a08bd8f660dc8ee4099894afbfbba6c99d0
Cristian Greco <cristianrgreco@gmail.com>
2025-05-13 13:22:52 +0100

    Fix chess.com case where no performance data available for game type

--- api/src/core/chesscom/parse-performance.js
+++ api/src/core/chesscom/parse-performance.js
@@ -5,11 +5,13 @@ export function parsePerformance(statsResponse, games, gameType, colour) {
   const currentStreak = calculateStreak(games, colour);
 
   return {
-    lowestRating: performance.last ? performance.last.rating - performance.last.rd : null, // rd = rating deviation
+    lowestRating: performance?.last ? performance.last.rating - performance.last.rd : null, // rd = rating deviation
     lowestRatingDateTime: null, // missing from response
     ...calculateHighestRating(performance),
-    currentRating: performance.last ? performance.last.rating : null,
-    totalNumberOfGames: performance.record.win + performance.record.loss + performance.record.draw,
+    currentRating: performance?.last ? performance.last.rating : null,
+    totalNumberOfGames: performance?.record
+      ? performance.record.win + performance.record.loss + performance.record.draw
+      : 0,
     totalNumberOfDisconnects: games.filter((game) => game[colour].result === "abandoned").length,
     currentLosingStreak: currentStreak < 0 ? -currentStreak : 0,
     currentWinningStreak: currentStreak > 0 ? currentStreak : 0,
@@ -18,15 +20,15 @@ export function parsePerformance(statsResponse, games, gameType, colour) {
 }
 
 function calculateHighestRating(performance) {
-  if (performance.best) {
+  if (performance?.best) {
     return {
       highestRating: performance.best.rating,
       highestRatingDateTime: chesscomDateToString(performance.best.date),
     };
   } else {
     return {
-      highestRating: performance.last ? performance.last.rating + performance.last.rd : null,
-      highestRatingDateTime: performance.last ? chesscomDateToString(performance.last.date) : null,
+      highestRating: performance?.last ? performance.last.rating + performance.last.rd : null,
+      highestRatingDateTime: performance?.last ? chesscomDateToString(performance.last.date) : null,
     };
   }
 }
